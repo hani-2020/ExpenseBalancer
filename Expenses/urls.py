463ddcecdbf1428ae36ba5a324390f1fc2a216ae
hani-2020 <haniroshan2002@gmail.com>
2024-07-30 19:14:43 +0530

    working on view expenses

--- Expenses/urls.py
+++ Expenses/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('create_expense/<int:id>', views.create_expense, name='create_expense'),
-    path('save_split/<int:expense_id>/', views.save_split, name='save_split')
+    path('save_split/<int:expense_id>/', views.save_split, name='save_split'),
+    path('view_expenses', views.view_expenses, name='view_expenses')
 ]
--- Expenses/views.py
+++ Expenses/views.py
@@ -3,6 +3,21 @@ from Groups.models import Group
 from .models import Expenses, Split
 from Authentications.models import User
 
+
+def save_split_helper(request, key, percents_or_amounts, expense, splits):
+    if key != 'csrfmiddlewaretoken':
+        rate_or_amount = request.POST.get(key)
+        if rate_or_amount:
+            rate_or_amount = float(rate_or_amount)
+            percents_or_amounts.append(rate_or_amount)
+            member = User.objects.get(id=key)
+            if expense.split_method==2:
+                amount = float(expense.amount) * rate_or_amount/100
+            if expense.split_method==3:
+                amount = rate_or_amount
+            split = Split(payer=member, expense=expense, amount=amount)
+            splits.append(split)
+
 def create_expense(request, id):
     group = Group.objects.get(id=id)
     members = group.members.all()
@@ -20,21 +35,60 @@ def create_expense(request, id):
         if not amount:
             context['error'] = "Amount field must not be empty"
             return render(request, 'create_expense.html', context)
-        expense = Expenses.objects.create(amount=amount, description=description, date=date, paid_by=paid_by, group=group, split_method=split_method)
+        expense = Expenses.objects.create(amount=amount,
+                                          description=description,
+                                          paid_by=paid_by, group=group,
+                                          split_method=split_method)
+        if date:
+            expense.date = date
         expense.save()
         context['expense'] = expense
-        if split_method=='1':
+        if split_method == '1':
             amount = int(amount)/len(members)
             context['amount'] = amount
         return render(request, 'split_details.html', context)
     return render(request, 'create_expense.html', context)
 
 def save_split(request, expense_id):
+    expense = Expenses.objects.get(id=expense_id)
+    members = expense.group.members.all()
+    context = {
+        'expense':expense,
+        'members':members
+    }
     if request.method=='POST':
-        expense = Expenses.objects.get(id=expense_id)
         if expense.split_method == 1:
-            members = expense.group.members.all()
             amount = expense.amount/len(members)
             for member in members:
                 Split.objects.create(payer=member, expense=expense, amount=amount)
-            return redirect('view_groups')
+        elif expense.split_method == 2:
+            percents = []
+            splits = []
+            for key in request.POST:
+                save_split_helper(request, key, percents, expense, splits)
+            if sum(percents) != 100:
+                context['error'] = 'Divison does not reach 100%'
+                return render(request, 'split_details.html', context)
+            for split in splits:
+                split.save()
+        elif expense.split_method == 3:
+            amounts  = []
+            splits = []
+            for key in request.POST:
+                save_split_helper(request, key, amounts, expense, splits)
+            if sum(amounts) != expense.amount:
+                context['error'] = 'Divison does not reach full amount'
+                return render(request, 'split_details.html', context)
+            for split in splits:
+                split.save()
+    return redirect('view_groups')
+
+def view_expenses(request):
+    splits =  Split.objects.filter(payer=request.user)
+    expenses = []
+    for split in splits:
+        expenses.append(split.expense)
+    context = {
+        'expenses':expenses
+    }
+    return render(request, 'view_expenses.html', context)
